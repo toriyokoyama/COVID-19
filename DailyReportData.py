cde3f5882c33c6d8498d9928921fed4d666c414c
toriyokoyama <toriyokoyama@gmail.com>
2020-03-25 22:08:33 -0500

    Update data to pull in China and Korea

--- DailyReportData.py
+++ DailyReportData.py
@@ -84,29 +84,41 @@ for i, file in enumerate(files):
         df = pd.read_csv(path + file)
         cols = df.columns
         df.columns = [col.replace('/','_').replace(' ','_').replace('Latitude','Lat').replace('Longitude','Long_') for col in cols]
-        df = df[(df['Country_Region']=='US')]
+        #df = df[(df['Country_Region']=='US')]
         df['File_Date'] = datetime.date(int(file[6:10]),int(file[0:2]),int(file[3:5]))
     else:
         new_df = pd.read_csv(path+file)
         cols = new_df.columns
         new_df.columns = [col.replace('/','_').replace(' ','_').replace('Latitude','Lat').replace('Longitude','Long_') for col in cols]
-        new_df = new_df[(new_df['Country_Region']=='US')]
+        #new_df = new_df[(new_df['Country_Region']=='US')]
         new_df['File_Date'] = datetime.date(int(file[6:10]),int(file[0:2]),int(file[3:5]))
         df = pd.concat((df,new_df),axis=0)
         
 # extract date from datetime
 df['Last_Update'] = pd.to_datetime(df['Last_Update']).dt.date
+# fix some historical country labeling
+df['Country_Region'].replace('China','Mainland China',inplace=True)
+df['Country_Region'].replace('Korea, South','South Korea',inplace=True)
+df['Country_Region'].replace('Republic of Korea','South Korea',inplace=True)
 # fill county name with default value
 df['Admin2'] = df['Admin2'].fillna('Unknown')
 # get city/area and state from data
+df['Province_State'] = df['Province_State'].fillna('')
 df['City_Area'] = df['Province_State'].apply(lambda x: x.split(', ')).apply(lambda x: x[0] if len(x)==2 else '')
 df['State'] = df['Province_State'].apply(lambda x: x.split(', ')).apply(lambda x: x[1] if len(x)==2 else x[0])
 df['State'] = df['State'].apply(lambda x: state_abbrev_inv.get(x,x))
 # make new key
 df['Key'] = (df['Admin2'] + ', ' + df['City_Area'] + ', ' + df['State']).apply(lambda x: x[2:] if x[0:2]==', ' else (x[4:] if x[0:4]==', , ' else x))
-groupByUS = df.groupby(['File_Date','Last_Update']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
+groupByUS = df[df['Country_Region']=='US'].groupby(['File_Date','Last_Update']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
 groupByUS_NoDup = groupByUS[groupByUS.duplicated(['Last_Update'])==False]
-groupByState = df.groupby(['File_Date','Last_Update','State']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
+groupByState = df[df['Country_Region']=='US'].groupby(['File_Date','Last_Update','State']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
 groupByState_NoDup = groupByState[groupByState.duplicated(['Last_Update','State'])==False]
-groupByAreaState = df.groupby(['File_Date','Last_Update','City_Area','State']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
+groupByAreaState = df[df['Country_Region']=='US'].groupby(['File_Date','Last_Update','City_Area','State']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
 groupByAreaState_NoDup = groupByAreaState[groupByAreaState.duplicated(['Last_Update','City_Area','State'])==False]
+
+groupByChina = df[df['Country_Region']=='Mainland China'].groupby(['File_Date','Last_Update']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
+groupByChina_NoDup = groupByChina[groupByChina.duplicated(['Last_Update'])==False]
+groupBySKorea = df[df['Country_Region']=='South Korea'].groupby(['File_Date','Last_Update']).sum()[['Confirmed','Deaths','Recovered']].reset_index()
+groupBySKorea_NoDup = groupBySKorea[groupBySKorea.duplicated(['Last_Update'])==False]
+
+# git merge upstream/master
